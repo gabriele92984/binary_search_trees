f7786d1d09e6060ad0c091d895f507a0a823fa31
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-03-31 15:57:10 +0200

    Create #build_tree method

--- bst.rb
+++ bst.rb
@@ -12,3 +12,31 @@ class Node
     @data <=> other.data
   end
 end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @root = build_tree(array.uniq.sort)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    mid = array.length / 2
+    root_node = Node.new(array[mid])
+
+    root_node.left = build_tree(array[0..mid])
+    root_node.right = build_tree(array[mid + 1..-1])
+
+    root_node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+  
+  
+end
