b2cfdc9495aa855cff21d1b46ac8252943922f7c
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-03-31 16:40:14 +0200

    Create #insert and #delete methods

--- bst.rb
+++ bst.rb
@@ -32,11 +32,45 @@ class Tree
     root_node
   end
 
+  def insert(value, node = @root)
+    return Mode.new(value) if node.nil?
+
+    if value < node.data
+      node.left = insert(value, node.left)
+    elsif value > node.data
+      node.right = insert(value.node.right)
+    end
+
+    node
+  end
+
+  def delete(value, node = @root)
+    return node if node.nil?
+
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+        node.right = delete(value, node.right)
+    else
+      # Node with only one child or no child
+      if node.left.nil?
+        return node.right
+      elsif node.right?
+        return node.left
+      end
+
+      # Node with two children: get the inorder successor (smallest in the right subtree)
+      temp = min_value_node(node.right)
+      node.data = temp.data
+      node.right = delete(temp.data, node.right)
+    end
+
+    node
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-  
-  
 end
